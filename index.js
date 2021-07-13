3d07e2bbdfbfacde1765d6581d07441cf057c2eb
Daniel <dannyblaze469@gmail.com>
2021-07-13 17:32:28 -0400

    added css and images

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
-console.log("Feeling Good by Nina Simone")
-// https://api.spoonacular.com/recipes/complexSearch
-fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=a47d599a4f91434786ca05a8cd3f632f")
-    .then(res => res.json())
-    .then(json => console.log(json.results[0]))
+document.addEventListener('DOMContentLoaded', () => {
+    fetchAllDishes()
+})
 
-// api key a47d599a4f91434786ca05a8cd3f632f
 
-// https://spoonacular.sendybay.com/l/aNQ892MfCedsWZ2k3763aF763g5A/kmEkER6LKZ2ET5MBNiEW8A/d0x7mPt9iieyOxNkulYj0w
+function renderAllDishes(foodObj){
+    console.log(foodObj)
+    let divContainer = document.createElement('div')
+    let divFrame = document.createElement('div')
+    let divImage = document.createElement('div')
+    let h1Discription = document.createElement('h2')
+    let h1Category = document.createElement('h1')
+    let img = document.createElement('img')
+
+    divContainer.className = 'meal-card'
+    divFrame.className = 'meal-frame'
+    divImage.className = 'meal-image'
+    h1Discription.className = 'center-text'
+
+    // console.log(img.src = foodObj.img)
+    h1Category.textContent = foodObj.strCategory
+    h1Discription.textContent = foodObj.strCategoryDescription
+    img.src = foodObj.strCategoryThumb
+
+    divImage.append(img)
+    divFrame.append(h1Category, divImage, h1Discription)
+    divContainer.append(divFrame)
+
+    document.querySelector('#meal-container').append(divContainer)
+}
+
+function fetchAllDishes(){
+    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+        .then(res => res.json())
+        // .then(json => console.log(json))
+        .then(json => json.categories.forEach(renderAllDishes))
+}
+
