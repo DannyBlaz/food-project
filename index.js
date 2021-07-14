0454995dae6b4ab46c76e41d9e6f84fa67356202
Daniel <dannyblaze469@gmail.com>
2021-07-14 08:48:45 -0400

    added event listeners

--- index.js
+++ index.js
@@ -4,29 +4,57 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 function renderAllDishes(foodObj){
-    console.log(foodObj)
+    // console.log(foodObj)
     let divContainer = document.createElement('div')
     let divFrame = document.createElement('div')
     let divImage = document.createElement('div')
     let h1Discription = document.createElement('h2')
     let h1Category = document.createElement('h1')
     let img = document.createElement('img')
-
+    
+    // like section
+    let foodLikes = 0
+    const likeContainer = document.createElement('div');
+    const likes = document.createElement('span');
+    const likeLogo = document.createElement('button');
+    
+    likeContainer.className = 'likes-section'
+    likes.className = 'likes'
+    likeLogo.className = 'like-button'
+    
+    likes.textContent = "0 likes"
+    likeLogo.textContent = "♥"
+    
+    
     divContainer.className = 'meal-card'
     divFrame.className = 'meal-frame'
     divImage.className = 'meal-image'
     h1Discription.className = 'center-text'
-
-    // console.log(img.src = foodObj.img)
+    
     h1Category.textContent = foodObj.strCategory
     h1Discription.textContent = foodObj.strCategoryDescription
     img.src = foodObj.strCategoryThumb
-
+    
     divImage.append(img)
-    divFrame.append(h1Category, divImage, h1Discription)
+    likeContainer.append(likes, likeLogo)
+    divFrame.append(h1Category, divImage, likeContainer)
     divContainer.append(divFrame)
 
     document.querySelector('#meal-container').append(divContainer)
+    
+    // Event Listeners
+    divImage.addEventListener('mouseover', (e) => {
+        divImage.style.cursor = 'pointer'
+    })
+
+    divImage.addEventListener('click', () => {
+        divFrame.append(h1Discription) 
+    })
+
+    document.querySelector('button.like-button').addEventListener('click', () => {
+        foodLikes += 1
+        document.querySelector('span.likes').textContent = `${foodLikes} likes`
+    })
 }
 
 function fetchAllDishes(){
